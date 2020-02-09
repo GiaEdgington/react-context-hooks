4fab3b6510c5831f347d7ea50cc2c943d3633871
Gia Edgington <gia.edgington@gmail.com>
2020-02-09 18:34:01 -0500

    Create BookList component

---
+++ src/components/BookList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default BookList;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,14 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() { 
         return ( 
-            <div></div>
+            <nav>
+                <h1>Context App</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
          );
     }
 }
