ba59b0fcb27acacc06cdfc689708628befe8372f
Gia Edgington <gia.edgington@gmail.com>
2020-02-14 23:26:57 -0500

    Pass data as value in Provider/ Wrap children components with Provider

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, Component } from 'react';
 
 export const ThemeContext = createContext();
 
@@ -10,8 +10,8 @@ class ThemeContextProvider extends Component {
     }
     render() { 
         return (
-            <ThemeContext.Provider>
-                
+            <ThemeContext.Provider value={{...this.state}}>
+                {this.props.children}
             </ThemeContext.Provider>
         );
     }
